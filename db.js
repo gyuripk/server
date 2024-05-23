0fa9c95b49060e1c8f949a525bb0ed727e942df0
rbfl6418 <rbfl6418@gmail.com>
2024-05-24 01:31:24 +1000

    added db info into env

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 module.exports = {
-  client: "mysql2",
+  client: process.env.DB_CLIENT,
   connection: {
-    host: "localhost",
-    database: "world",
-    user: "root",
-    password: "Gksmf1124.",
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   },
 };
